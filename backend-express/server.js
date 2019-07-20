65892e612c94d2ac837c92d0288d022c1cd23845
Alimurrazi <alimurrazi570@gmail.com>
2019-07-20 11:27:59 +0600

    connection url and endpoint update

--- backend-express/server.js
+++ backend-express/server.js
@@ -10,7 +10,8 @@ const router = express.Router();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://[server]/issues');
+//mongoose.connect('mongodb://[server]/issues');
+mongoose.connect('mongodb://localhost:27017/issues');
 const connection = mongoose.connection;
 
 connection.once('open',()=>{
@@ -42,7 +43,32 @@ router.route('/issues/add').post((req, res) => {
             res.status(400).send('Failed to create new record');
         });
 });
-
+router.route('/issues/update/:id').post((req, res) => {
+    Issue.findById(req.params.id, (err, issue) => {
+        if (!issue)
+            return next(new Error('Could not load Document'));
+        else {
+            issue.title = req.body.title;
+            issue.responsible = req.body.responsible;
+            issue.description = req.body.description;
+            issue.severity = req.body.severity;
+            issue.status = req.body.status;
+            issue.save().then(issue => {
+                res.json('Update done');
+            }).catch(err => {
+                res.status(400).send('Update failed');
+            });
+        }
+    });
+});
+router.route('/issues/delete/:id').get((req, res) => {
+    Issue.findByIdAndRemove({_id: req.params.id}, (err, issue) => {
+        if (err)
+            res.json(err);
+        else
+            res.json('Removed successfully');
+    });
+});
 
 app.use('/', router);
 //app.get('/', (req, res) => res.send('Hello World!'));
